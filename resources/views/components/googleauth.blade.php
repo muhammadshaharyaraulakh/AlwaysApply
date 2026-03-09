cc3564cfa170a024db60ce4fa3de81a56ec75232
muhammadshaharyaraulakh <muhammadshaharyaraulakh@gmail.com>
2026-03-09 13:50:05 +0500

    Refactor routes and add new views components

---
+++ resources/views/components/googleauth.blade.php
@@ -0,0 +1,10 @@
+ <a
+            href="/login/google"
+            class="p-3 bg-white border cursor-pointer border-gray-200 rounded-full shadow-sm hover:shadow-md transition inline-flex items-center justify-center"
+          >
+            <img
+              src="/frontend/images/google.svg"
+              alt="Google"
+              class="w-8 h-8"
+            />
+          </a>
